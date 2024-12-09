566fad1ed474a61ba675b5911aef348b0d152990
Amin Aouina <91031103+silamin@users.noreply.github.com>
2024-12-10 00:56:49 +0100

    add item

--- SEP3_T1_BlazorUI/Application/UseCases/ItemUseCases.cs
+++ SEP3_T1_BlazorUI/Application/UseCases/ItemUseCases.cs
@@ -1,5 +1,8 @@
-﻿using SEP3_T1_BlazorUI.Models;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using SEP3_T1_BlazorUI.Application.Interfaces;
+using SEP3_T1_BlazorUI.Models;
 
 namespace SEP3_T1_BlazorUI.Application.UseCases
 {
@@ -12,18 +15,27 @@ namespace SEP3_T1_BlazorUI.Application.UseCases
             _itemRepository = itemRepository;
         }
 
-        public void AddItem(Item item)
+        public async Task AddItemAsync(Item item)
         {
+            await Task.Delay(1000); // Simulate network delay
             _itemRepository.AddItem(item);
+        }
+
+        public async Task<Item> FindItemByNameAndDescriptionAsync(string name, string description)
+        {
+            await Task.Delay(500); // Simulate network delay
+            return _itemRepository.GetAllItems().FirstOrDefault(i => i.Name.Equals(name, System.StringComparison.OrdinalIgnoreCase)
+                                                && i.Description.Equals(description, System.StringComparison.OrdinalIgnoreCase));
         }
 
         public void DeleteItem(Item item)
         {
             _itemRepository.DeleteItem(item);
         }
-        public void UpdateItem(Item item) {
-            _itemRepository.UpdateItem(item);
 
+        public void UpdateItem(Item item)
+        {
+            _itemRepository.UpdateItem(item);
         }
 
         public IEnumerable<Item> GetAllItems()
