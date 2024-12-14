78bb787a2dd11e88dec8ea7e78f007057418cb06
Amin Aouina <91031103+silamin@users.noreply.github.com>
2024-12-14 08:23:23 +0100

    refactor

--- SEP3_T3_AspCoreWebApi/SEP3_T3_ASP_Core_WebAPI/SEP3_T3_ASP_Core_WebAPI/ApiContracts/UserDto/UserResponse.cs
+++ SEP3_T3_AspCoreWebApi/SEP3_T3_ASP_Core_WebAPI/SEP3_T3_ASP_Core_WebAPI/ApiContracts/UserDto/UserResponse.cs
@@ -1,6 +1,6 @@
 ﻿namespace SEP3_T3_ASP_Core_WebAPI.ApiContracts.UserDto
 {
-    public class UserDto
+    public class UserResponse
     {
         public int UserId { get; set; }
         public string? UserName { get; set; }
