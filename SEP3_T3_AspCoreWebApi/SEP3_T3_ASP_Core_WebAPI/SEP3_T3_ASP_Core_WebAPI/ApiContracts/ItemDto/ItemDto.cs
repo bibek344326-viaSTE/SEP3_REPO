2f7afaffd8d8b337d5e66ef65f588cc18bfc14e4
Amin Aouina <91031103+silamin@users.noreply.github.com>
2024-12-14 07:59:06 +0100

    ref

--- SEP3_T3_AspCoreWebApi/SEP3_T3_ASP_Core_WebAPI/SEP3_T3_ASP_Core_WebAPI/ApiContracts/ItemDto/ItemDto.cs
+++ SEP3_T3_AspCoreWebApi/SEP3_T3_ASP_Core_WebAPI/SEP3_T3_ASP_Core_WebAPI/ApiContracts/ItemDto/ItemDto.cs
@@ -1,8 +1,7 @@
 ﻿namespace SEP3_T3_ASP_Core_WebAPI.ApiContracts.ItemDto;
 
-public class ItemDto
+public class CreateItemDto
 {
-    public required int ItemId { get; set; }
     public required string ItemName { get; set; }
     public string? Description { get; set; }
     public int QuantityInStore { get; set; }
