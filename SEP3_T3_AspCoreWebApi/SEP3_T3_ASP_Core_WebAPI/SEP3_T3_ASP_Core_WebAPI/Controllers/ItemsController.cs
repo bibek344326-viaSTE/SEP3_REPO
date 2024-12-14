3bf65cdb7827e57947b27d4b133b88ca9bd34a67
Amin Aouina <91031103+silamin@users.noreply.github.com>
2024-12-14 08:25:11 +0100

    minor changes item controller

--- SEP3_T3_AspCoreWebApi/SEP3_T3_ASP_Core_WebAPI/SEP3_T3_ASP_Core_WebAPI/Controllers/ItemsController.cs
+++ SEP3_T3_AspCoreWebApi/SEP3_T3_ASP_Core_WebAPI/SEP3_T3_ASP_Core_WebAPI/Controllers/ItemsController.cs
@@ -92,7 +92,7 @@ public class ItemsController: ControllerBase
             itemToUpdate.QuantityInStore = request.QuantityInStore;
             
             await itemRepository.UpdateItemAsync(itemToUpdate);
-            return NoContent();
+            return Ok(itemToUpdate);
         }
         catch (InvalidOperationException)
         {
@@ -115,7 +115,7 @@ public class ItemsController: ControllerBase
             itemToUpdate.QuantityInStore = request.QuantityInStore;
             
             await itemRepository.UpdateItemAsync(itemToUpdate);
-            return NoContent();
+            return Ok(itemToUpdate);
         }
         catch (InvalidOperationException)
         {
@@ -138,7 +138,7 @@ public class ItemsController: ControllerBase
             itemToUpdate.Description = request.Description;
             
             await itemRepository.UpdateItemAsync(itemToUpdate);
-            return NoContent();
+            return Ok(itemToUpdate);
         }
         catch (InvalidOperationException)
         {
@@ -161,7 +161,7 @@ public class ItemsController: ControllerBase
             itemToUpdate.ItemName = request.ItemName;
             
             await itemRepository.UpdateItemAsync(itemToUpdate);
-            return NoContent();
+            return Ok(itemToUpdate);
         }
         catch (InvalidOperationException)
         {
@@ -181,7 +181,7 @@ public class ItemsController: ControllerBase
         try
         {
             await itemRepository.DeleteItemAsync(id);
-            return NoContent();
+            return Ok(true);
         }
         catch (InvalidOperationException)
         {
