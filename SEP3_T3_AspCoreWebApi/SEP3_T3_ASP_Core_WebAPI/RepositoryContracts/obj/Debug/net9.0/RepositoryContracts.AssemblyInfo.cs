5e38714ff6da5a23639e43e4265e6b79150fb88f
Amin Aouina <91031103+silamin@users.noreply.github.com>
2024-12-16 06:40:13 +0100

    sila

--- SEP3_T3_AspCoreWebApi/SEP3_T3_ASP_Core_WebAPI/RepositoryContracts/obj/Debug/net9.0/RepositoryContracts.AssemblyInfo.cs
+++ SEP3_T3_AspCoreWebApi/SEP3_T3_ASP_Core_WebAPI/RepositoryContracts/obj/Debug/net9.0/RepositoryContracts.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RepositoryContracts")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e24c3a6e74f73c1d7cf01d3f49091017d6b00d7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d8aa5e71afeb9b4b8a9ecbb6c7ad6def46a0e07")]
 [assembly: System.Reflection.AssemblyProductAttribute("RepositoryContracts")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RepositoryContracts")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
